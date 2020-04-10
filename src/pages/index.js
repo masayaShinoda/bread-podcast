ac22ed4d1ee6876cea8afce216bd1ba1c5e58065
masayaShinoda- <46096911+masayaShinoda@users.noreply.github.com>
2020-04-10 20:14:48 +0700

    deleted html.js and ran gatsby build locally before submitting

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 //images import
 import imgHomepage from "../images/homepage-BP.png"
-import introAnimationGif from "../gifs/intro-animation.gif"
-
-import cosha from "cosha"
-
-cosha({ className: "cool-shadow" })
 
 const IndexPage = () => (
   <Layout>

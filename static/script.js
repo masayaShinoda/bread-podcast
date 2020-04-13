028a15d0269a985ab88773cca806ca8b88bed7ff
masayaShinoda- <46096911+masayaShinoda@users.noreply.github.com>
2020-04-13 18:00:19 +0700

    Revert "updated script.js with if()"
    This reverts commit 44b17e451b6110e1bfd67247641ac2c2c0006d6c.

--- static/script.js
+++ static/script.js
@@ -3,12 +3,10 @@ console.log("Custom script is working!")
 const goDark = () => {
   const parent = document.querySelector(".parent.light")
   const goDarkBtn = document.querySelector("#goDark")
-  if (goDarkBtn) {
-    goDarkBtn.addEventListener("click", function() {
-      //after parent is switched to dark, change moon to sun
-      parent.classList.toggle("dark")
-    })
-  }
+  goDarkBtn.addEventListener("click", function() {
+    //after parent is switched to dark, change moon to sun
+    parent.classList.toggle("dark")
+  })
 }
 
 goDark()

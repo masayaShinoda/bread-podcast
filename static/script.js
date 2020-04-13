44b17e451b6110e1bfd67247641ac2c2c0006d6c
masayaShinoda- <46096911+masayaShinoda@users.noreply.github.com>
2020-04-13 16:32:57 +0700

    updated script.js with if()

--- static/script.js
+++ static/script.js
@@ -3,10 +3,12 @@ console.log("Custom script is working!")
 const goDark = () => {
   const parent = document.querySelector(".parent.light")
   const goDarkBtn = document.querySelector("#goDark")
-  goDarkBtn.addEventListener("click", function() {
-    //after parent is switched to dark, change moon to sun
-    parent.classList.toggle("dark")
-  })
+  if (goDarkBtn) {
+    goDarkBtn.addEventListener("click", function() {
+      //after parent is switched to dark, change moon to sun
+      parent.classList.toggle("dark")
+    })
+  }
 }
 
 goDark()

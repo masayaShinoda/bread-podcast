fbd792a347de30661d72a3411838e853b70e1259
masayaShinoda- <46096911+masayaShinoda@users.noreply.github.com>
2020-04-19 21:46:19 +0700

    - removed static folder - added .specialNav to  about, blog, and index.

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,10 @@ import SEO from "../components/seo"
 
 //styles import
 import aboutStyles from "../styles/styles.css"
+//icons import
+import homeIcon from "../assets/home-icon.png"
+import podcastsIcon from "../assets/podcasts-icon.png"
+import blogIcon from "../assets/blog-icon.png"
 
 const AboutPage = () => {
   //declare data variable, which is dynamic in order to change the <h1> of about.js
@@ -22,30 +26,43 @@ const AboutPage = () => {
   return (
     <Layout>
       <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
-      <div className="aboutContainer">
-        <div className="flexAbout">
-          <h1>About {data.site.siteMetadata.title}</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <div className="flexContact">
-          <h2>Contact:</h2>
-          <a
-            href="mailto:info@breaddesignstudio.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa fa-envelope"></i>
-            <p>info@breaddesignstudio.com</p>
-          </a>
-          <span>
-            <i className="fa fa-map-pin"></i>
-            <p>Phnom Penh, Cambodia.</p>
-          </span>
+      <div className="pageContainer">
+        <section className="specialNav">
+          <Link to="/">
+            <img src={homeIcon} title="Home"></img>
+          </Link>
+          <Link to="/about">
+            <img src={podcastsIcon} title="Podcasts"></img>
+          </Link>
+          <Link to="/blog">
+            <img src={blogIcon} title="Blog"></img>
+          </Link>
+        </section>
+        <div className="aboutContainer">
+          <div className="flexAbout">
+            <h1>About {data.site.siteMetadata.title}</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="flexContact">
+            <h2>Contact:</h2>
+            <a
+              href="mailto:info@breaddesignstudio.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa fa-envelope"></i>
+              <p>info@breaddesignstudio.com</p>
+            </a>
+            <span>
+              <i className="fa fa-map-pin"></i>
+              <p>Phnom Penh, Cambodia.</p>
+            </span>
+          </div>
         </div>
       </div>
     </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,26 @@ import indexStyles from "../styles/styles.css"
 
 //images import
 import imgHomepage from "../images/homepage-BP.png"
+//icons import
+import homeIcon from "../assets/home-icon.png"
+import podcastsIcon from "../assets/podcasts-icon.png"
+import blogIcon from "../assets/blog-icon.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="homepageContainer">
+    <div className="pageContainer">
+      <section className="specialNav">
+        <Link to="/">
+          <img src={homeIcon} title="Home"></img>
+        </Link>
+        <Link to="/about">
+          <img src={podcastsIcon} title="Podcasts"></img>
+        </Link>
+        <Link to="/blog">
+          <img src={blogIcon} title="Blog"></img>
+        </Link>
+      </section>
       <img src={imgHomepage} alt="Coming soon" className="homePageImage"></img>
     </div>
   </Layout>
